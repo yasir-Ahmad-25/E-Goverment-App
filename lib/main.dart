b95679eda91021261b2545efae16c6746d82a181
YASIR AHMAD <yazirahmed123@gmail.com>
2025-04-27 06:46:43 +0300

    styled the home page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:e_govermenet/screens/home_screen.dart';
 import 'package:e_govermenet/screens/login_screen.dart';
 import 'package:e_govermenet/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,10 +16,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Future<bool> _checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final citizenId = prefs.getInt('user_id');
+    return citizenId != null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: FutureBuilder(
+        future: _checkLoginStatus(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator.adaptive());
+          } 
+          return snapshot.data == true ? HomeScreen() : LoginScreen();
+        },
+      ),
       routes: {
         'sign_up': (context) => SignUpScreen(),
         'home_screen': (context) => HomeScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:icons_plus/icons_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,8 +14,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   String? _fullName;
+  String? _gender;
+  String? _userStatus;
+  bool _isActive = false;
   bool _isLoading = true;
 
   // Logout
@@ -38,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-   @override
+  @override
   void initState() {
     super.initState();
     _loadCitizenData();
@@ -65,6 +69,12 @@ class _HomeScreenState extends State<HomeScreen> {
         if (data['status'] == 'success') {
           setState(() {
             _fullName = data['data']['fullname'];
+            _gender = data['data']['gender'];
+            _userStatus = data['data']['status'];
+            if (_userStatus == 'Active') {
+              _isActive = true;
+            }
+
             _isLoading = false;
           });
         } else {
@@ -73,31 +83,124 @@ class _HomeScreenState extends State<HomeScreen> {
       } else {
         throw Exception("Server error: ${response.statusCode}");
       }
-
     } catch (e) {
       setState(() {
         _isLoading = false;
       });
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error: ${e.toString()}")),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error: ${e.toString()}")));
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("$_fullName"),
+        title: Text("Welcome , $_fullName 🔥", style: TextStyle(fontSize: 16)),
         actions: [
           IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () => _logout(),
+            icon: const Icon(Bootstrap.person_circle),
+            onPressed: () {
+              if (kDebugMode) {
+                print("Profile Icon Has Been Clicked");
+              }
+            },
           ),
         ],
       ),
-      body: Center(child: Text("Welcome To Home Citizen 🔥")),
+      body:
+          _isActive
+              ? Center(child: Text("Welcome Home Citizen 🔥"))
+              : PendingUser(gender: _gender.toString(), function: _logout),
     );
+  }
+}
+
+class PendingUser extends StatelessWidget {
+  final String gender;
+  final VoidCallback function;
+  const PendingUser({super.key, required this.gender, required this.function});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      /** Card Widget **/
+      child: Card(
+        elevation: 25,
+        shadowColor: Colors.black,
+        color: Colors.white,
+        child: SizedBox(
+          width: 300,
+          height: 400,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 70,
+                  child: const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      // "https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png",
+                      "https://www.ecss-online.com/2013/wp-content/uploads/2022/12/somalia.jpg",
+                    ), //NetworkImage
+                    radius: 100,
+                  ), //CircleAvatar
+                ), //CircleAvatar
+                const SizedBox(height: 10), //SizedBox
+                Text(
+                  'Salaan ${(gender == 'Male') ? 'Mudane' : 'Marwo'}',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.blue[900],
+                    fontWeight: FontWeight.w500,
+                  ), //Textstyle
+                ), //Text
+                const SizedBox(height: 10), //SizedBox
+                const Text(
+                  'Account Sida ugu dhaqsiyaha badan ayaa la xaqiijin doona ? Waaku Mahadsantahay Waqtigaaga.',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                  ), //Textstyle
+                ), //Text
+                const SizedBox(height: 15), //SizedBox
+                Center(
+                  child: ElevatedButton(
+                    onPressed: function,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Colors.redAccent,
+                      ),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      padding: WidgetStateProperty.all(
+                        const EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 12,
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min, // Prevents stretching
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Bootstrap.box_arrow_left, color: Colors.white),
+                        SizedBox(width: 8),
+                        Text('Logout', style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ), //Column
+          ), //Padding
+        ), //SizedBox
+      ), //Card
+    ); //Center
   }
 }
