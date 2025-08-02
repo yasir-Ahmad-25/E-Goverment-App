b5be88b9e238b2109d92ab6a743da404e439aae4
mokhader1001 <mokhader1001@gmail.com>
2025-08-02 11:51:32 +0300

    Save local changes before pulling

--- lib/components/services/api_constants.dart
+++ lib/components/services/api_constants.dart
@@ -1,7 +1,7 @@
 // lib/services/api_constants.dart
 class ApiConstants {
   //static const String baseUrl = 'http:// 10.203.28.39/Som-Gov';
-  static const String baseUrl = 'http://192.168.100.10/Som-Gov';
+  static const String baseUrl = 'http:// 172.20.10.2:8080/Som-Gov';
   // static const String baseUrl = 'http://192.168.202.39/Som-Gov';
   static const String nationalIdEndpoint = '/national_id';
   static const String passportEndpoint = '/passport_id';
